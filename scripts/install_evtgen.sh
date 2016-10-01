2d80c94891fa0d8217d87a0b078b4f2e844b0fc2
Thomas <Thomas.Ruf@cern.ch>
2016-10-01 14:53:32 +0200

    fix installation of evtgen on modern (w/o g77) linux systems

--- scripts/install_evtgen.sh
+++ scripts/install_evtgen.sh
@@ -24,14 +24,19 @@ if [ ! -d  $SIMPATH/generators/EvtGen ];then
  cd $EVTGEN_VERSION
 # -llhapdfdummy does not exist anymore for newer versions of Pythia8!
  mysed "-llhapdfdummy" " " configure
+ tmp=$(locate gfortran)
+ echo $tmp
+ if [[ "$tmp" == *"gfortran"* ]];
+ then 
+   mysed "-lfrtbegin -lg2c" " " configure
+ fi 
  ./configure --hepmcdir=$SIMPATH_INSTALL --photosdir=$SIMPATH_INSTALL --pythiadir=$SIMPATH_INSTALL --tauoladir=$SIMPATH_INSTALL
  make
 
+ cp lib/libEvtGenExternal.so $SIMPATH_INSTALL/lib
+ cp lib/libEvtGen.so $SIMPATH_INSTALL/lib
+
  echo Setup done.
- echo To complete, add the following command to your .bashrc file or run it in your terminal before running any programs that use the EvtGen library:
- echo LD_LIBRARY_PATH=$INSTALL_BASE/external/HepMC/lib:$INSTALL_BASE/external/pythia8186/lib:$INSTALL_BASE/external/PHOTOS/lib:$INSTALL_BASE/external/TAUOLA/lib:$INSTALL_BASE/ $VERSION/lib:\$LD_LIBRARY_PATH 
- echo Also set the Pythia8 data path:
- echo PYTHIA8DATA=$SIMPATH_INSTALL/share/pythia8/xmldoc
 fi
 
 cd $SIMPATH
